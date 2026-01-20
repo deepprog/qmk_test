a2ec737df8af01e2c22b47b65a43c159056bad91
deepprog <deepprog@gmail.com>
2026-01-20 20:28:16 +0900

    keymap.c を更新

--- keymap.c
+++ keymap.c
@@ -16,17 +16,16 @@
 #include QMK_KEYBOARD_H
 
 // Defines the keycodes used by our macros in process_record_user
-enum layer_names {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _ADJUST
-};
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 16
 
 enum custom_keycodes {
-  LOWER = SAFE_RANGE,
+  QWERTY = SAFE_RANGE,
+  LOWER,
   RAISE,
-  ADJUST,
+  ADJUST,     
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -40,77 +39,84 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               | GUI  | LOWER|Ctrl/Esc|          |Spc/Sft| RAISE|Alt/BkSp |
+ *               | Ctrl | LOWER|  Sp  |           |  Sp  | RAISE| Shift|
  *               `--------------------'           `--------------------'
  */
-[_QWERTY] = LAYOUT_split_3x5_3(
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-                    KC_LGUI, LOWER, MT(MOD_LCTL, KC_ESC),   MT(MOD_LSFT, KC_SPC), RAISE, MT(MOD_LALT, KC_BSPC)
+[_QWERTY] = LAYOUT( \
+  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+                   KC_LCTL,  LOWER,   KC_SPC,       KC_SPC,  RAISE,   KC_LSFT \
 ),
 
-/* Raise
+/* Raise テンキーと記号
  *
  * ,----------------------------------.           ,----------------------------------.
- * |   !  |   @  |   #  |   $  |   %  |           |   ^  |   &  |   *  |   (  |   )  |
+ * |  `   |  &   |  *   |  (   |  )   |           |  =+  |  7&  |  8*  |  9(  |  0)  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab |   _  |   +  |  |   |  ~   |           |   :  |   "  |   >  |   {  |   }  |
+ * |      |  $   |  %   |  ^   |      |           |  '"  |  4$  |  5%  |  6^  |  \|  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Caps|   -  |   =  |  \   |  `   |           |   ;  |   '  |   <  |   [  |   ]  |
+ * |  (   |  !   |  @   |  #   |  )   |           |  [{  |  1!  |  2@  |  3#  |  ]}  |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | LOWER|      |           |  Esc | RAISE|      |
+ *               | Ctrl |      | Shift|           |  0)  |@@@@@@| Win  |
  *               `--------------------'           `--------------------'
  */
-[_RAISE] = LAYOUT_split_3x5_3(
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-  KC_TAB,  KC_UNDS, KC_PLUS, KC_PIPE, KC_TILD,      KC_COLN, KC_DQUO, KC_GT,   KC_LCBR, KC_RCBR,
-  KC_CAPS, KC_MINS, KC_EQL,  KC_BSLS, KC_GRV,       KC_SCLN, KC_QUOT, KC_LT,   KC_LBRC, KC_RBRC,
-                    _______, _______, _______,      _______,  _______, _______
+[_RAISE] = LAYOUT( \
+  KC_GRV,  S(KC_7), S(KC_8), S(KC_9), S(KC_0),      KC_EQL,  KC_7, KC_8,  KC_9, KC_0,  \
+  _______, S(KC_4), S(KC_5), S(KC_6), _______,      KC_QUOT, KC_4, KC_5,  KC_6, KC_BSLS, \
+  KC_LPRN, S(KC_1), S(KC_2), S(KC_3), KC_RPRN,      KC_LBRC, KC_1, KC_2,  KC_3, KC_RBRC, \
+                    KC_LCTL, _______, KC_LSFT,      KC_0,    _______, KC_LGUI  \
 ),
 
-/* Lower
+/* Lower 制御系と左手テンキー
  *
  * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
+ * | Esc  |  7&  |  8*  |  9(  |  0)  |           | Home | BkSp |  -_  |  Del | Esc  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab |      |      |      |      |           | Left | Down |  Up  | Right| Enter|
+ * | Tab  |  4$  |  5%  |  6^  |      |           | Left | Up   | Right|      | Ent  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |  GUI |  Alt | Del  |           | BkSp | PgUp | PgDn |   \  |   '  |
+ * |  (   |  1!  |  2@  |  3#  |   )  |           | End  | Down | Tab  |      |      |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | LOWER|      |           |      | RAISE|      |
+ *               |  0)  |@@@@@@|      |           | Shift|      | Win  |
  *               `--------------------'           `--------------------'
  */
-[_LOWER] = LAYOUT_split_3x5_3(
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_TAB,  _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-  KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, KC_DEL,      KC_BSPC, KC_PGUP, KC_PGDN, KC_BSLS, KC_QUOT,
-                    _______, _______, _______,      _______, _______, _______
+[_LOWER] = LAYOUT( \
+  KC_ESC,  KC_7, KC_8, KC_9, KC_0,           KC_HOME, KC_BSPC, KC_MINS, KC_DEL,  KC_ESC,  \
+  KC_TAB,  KC_4, KC_5, KC_6, _______,        KC_LEFT, KC_UP,   KC_RGHT, _______, KC_ENT,  \
+  S(KC_9), KC_1, KC_2, KC_3, S(KC_0),        KC_END,  KC_DOWN, KC_TAB,  _______, _______, \
+                 KC_0, _______, _______,     KC_LSFT, _______, KC_LGUI \
 ),
 
 
-/* Adjust (Lower + Raise)
+/* Adjust (Lower + Raise) Fキーと特殊
  *
  * ,----------------------------------.           ,----------------------------------.
- * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  F8  |  F9  |  F10 |
+ * | Reset|      |      |      |      |           |      |  F7  |  F8  |  F9  | Reset|
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      |      |      |           |      |      |      |      |      |
+ * |      |      | Vol- | Mute | Vol+ |           |      |  F4  |  F5  |  F6  |  F11 |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * | Reset|      |      |      |      |           | Prev | Next | Vol- | Vol+ | Play |
+ * |      |      | Prev | Play | Next |           |      |  F1  |  F2  |  F3  |  F12 |
  * `-------------+------+------+------|           |------+------+------+------+------'
- *               |      | LOWER|      |           |      | RAISE|      |
+ *               |      |      |      |           |  F10 |      |      |
  *               `--------------------'           `--------------------'
  */
-[_ADJUST] =  LAYOUT_split_3x5_3(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, _______, _______,
-  QK_BOOT, _______, _______, _______, _______,      KC_MPRV, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY,
-                    _______, _______, _______,      _______,  _______, _______
+[_ADJUST] =  LAYOUT( \
+  RESET,   _______, _______, _______, _______,      _______, KC_F7,   KC_F8,   KC_F9,   RESET,   \
+  _______, _______, KC_VOLD, KC_MUTE, KC_VOLU,      _______, KC_F4,   KC_F5,   KC_F6,   KC_F11, \
+  _______, _______, KC_MPRV, KC_MPLY, KC_MNXT,      _______, KC_F1,   KC_F2,   KC_F3,   KC_F12,  \
+                    _______, _______, _______,      KC_F10,  _______, _______  \
 )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        // persistant_default_layer_set(1UL<<_QWERTY);
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
     case LOWER:
       if (record->event.pressed) {
         layer_on(_LOWER);
@@ -143,3 +149,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+}
+
+void led_set_user(uint8_t usb_led) {
+}
